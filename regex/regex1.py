5bcf375173920604428af3a7e1bcba855e317057
sudhakar <sudhakarsvnt96@gmail.com>
2020-05-18 17:28:46 +0530

    regular expression examples

---
+++ regex/regex1.py
@@ -0,0 +1,38 @@
+import re
+
+str='this is my mob no +917272727272'
+mobregx = re.compile(r'\+\d\d\d\d\d\d\d\d\d\d\d\d')
+mob = mobregx.search(str)
+print(mob)
+print(mob.group())
+
+str='this is my mob no +917272727272 and ofc no is +917272727373'
+mo2=mobregx.findall(str)
+for i in mo2:
+    print(i)   
+
+# output
+# <re.Match object; span=(18, 31), match='+917272727272'>
+# +917272727272
+# +917272727272
+# +917272727373
+
+#------------------
+str='this is my mob no +917272727272'
+mob = re.search(r'(\+\d\d)(\d\d\d\d\d\d\d\d\d\d)',str)
+print("mobile no:",mob.group())
+print("Country code:", mob.group(1))
+print("phone no:",mob.group(2))
+
+#--------3rd way
+def getPhoneNumber(s):
+   
+    mobregx = re.compile(r'(\+\d\d)(\d\d\d\d\d\d\d\d\d\d)')
+    mob = mobregx.findall(str)
+    if mob:
+        return mob
+    else:
+        return None
+
+str='this is my mob no +917272727272'
+print(getPhoneNumber(str))
